263544b48f06e324a0d79fe6dab037eef69a5e08
Soumya Upadhyay <soumyaupadhya121@gmail.com>
2026-03-09 00:03:26 +0530

    Question

--- 1.Arrays/1.Traversal-Pattern/1.Basic Iteration/_2_Max_ele.java
+++ 1.Arrays/1.Traversal-Pattern/1.Basic Iteration/_2_Max_ele.java
@@ -10,7 +10,17 @@ public class _2_Max_ele {
             // 9
             // Hint
             // Start with the first element as max and compare it with each next element.
-            Scanner sc=new Scanner(System.in);
-            int n=
+            int n=sc.nextInt();
+            int arr[]=new int[n];
+            for(int i=0;i<n;i++){
+                arr[i]=sc.nextInt();
+            }
+            int max=arr[0];
+            for(int ele:arr){
+                if(ele>max){
+                    max=ele;
+                }
+            }
+            System.out.println(max);
     }
 }
