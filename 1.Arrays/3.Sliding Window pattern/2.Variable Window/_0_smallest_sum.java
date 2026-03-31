caa84bc9174b3617529bf577d7f91d581cb01685
Soumya Upadhyay <soumyaupadhya121@gmail.com>
2026-03-31 22:55:51 +0530

    Q2

---
+++ 1.Arrays/3.Sliding Window pattern/2.Variable Window/_0_smallest_sum.java
@@ -0,0 +1,35 @@
+import java.util.Scanner;
+
+public class _0_smallest_sum {
+    public static void main(String[] args) {
+        // Description
+        // Find the smallest subarray whose sum ≥ target.
+        // Sample Input
+        // 6
+        // 2 3 1 2 4 3
+        // 7
+        // Sample Output
+        // 2
+        // Hint
+        // Shrink window once sum reaches target.
+        Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt();
+        int arr[]=new int[n];
+        for(int i=0;i<n;i++){
+            arr[i]=sc.nextInt();
+        }
+        int k=sc.nextInt();
+        int l=0,r=0,minlen=Integer.MAX_VALUE,sum=0;
+         while(r<n){
+            sum+=arr[r];
+            while(sum>=k){
+                minlen=Math.min(minlen,r-l+1);
+                sum-=arr[l++];
+            }
+            r++;
+        }
+        System.out.println(minlen);
+        // Longest subarray → shrink when condition breaks ❌
+        //Smallest subarray → shrink when condition satisfies ✅
+    }
+}
