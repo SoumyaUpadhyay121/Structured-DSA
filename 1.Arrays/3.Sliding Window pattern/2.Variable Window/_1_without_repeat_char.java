09f49e15327080689f9ad97c9e2968a5224d96b9
Soumya Upadhyay <soumyaupadhya121@gmail.com>
2026-04-01 23:55:09 +0530

    Q3

---
+++ 1.Arrays/3.Sliding Window pattern/2.Variable Window/_1_without_repeat_char.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class _1_without_repeat_char {
+    public static void main(String[] args) {
+        // Description
+        // Find the longest substring without repeating characters.
+        // Sample Input
+        // abcabcbb
+        // Sample Output
+        // 3
+        // Hint
+        // Use a set to track characters in the current window.
+
+        Scanner sc=new Scanner(System.in);
+        String s=sc.next();
+
+        int l=0,r=0,maxlen=Integer.MIN_VALUE;
+        while(r<s.length()){
+            
+        }
+    }
+}
