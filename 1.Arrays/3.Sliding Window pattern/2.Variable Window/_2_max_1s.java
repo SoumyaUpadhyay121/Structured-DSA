a0e2bc28df8c12c401326afbbcd864466501a87f
Soumya Upadhyay <soumyaupadhya121@gmail.com>
2026-04-03 23:13:20 +0530

    Q4

--- 1.Arrays/3.Sliding Window pattern/2.Variable Window/_2_max_1s.java
+++ 1.Arrays/3.Sliding Window pattern/2.Variable Window/_2_max_1s.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 public class _2_max_1s {
     public static void main(String[] args) {
         // Description
@@ -10,6 +12,28 @@ public class _2_max_1s {
         // 4
         // Hint
         // Track number of zeros in the window.
-        
+         Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt();
+        int arr[]=new int[n];
+        for(int i=0;i<n;i++){
+            arr[i]=sc.nextInt();
+        }
+        int k=sc.nextInt();
+        int left = 0, zeros = 0, maxLen = 0;
+        for (int right = 0; right < arr.length; right++) {
+            if (arr[right] == 0) {
+                zeros++;
+            }
+
+            // if too many zeros, shrink window
+            while (zeros > k) {
+                if (arr[left] == 0) {
+                    zeros--;
+                }
+                left++;
+            }
+            maxLen = Math.max(maxLen, right - left + 1);
+        }
+        System.out.println(maxLen);
     }
 }
