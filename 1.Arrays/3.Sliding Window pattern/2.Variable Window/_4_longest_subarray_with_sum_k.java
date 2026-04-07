31e5e452ea33c6691a8c6f5938e2e842b773c32c
Soumya Upadhyay <soumyaupadhya121@gmail.com>
2026-04-07 23:24:14 +0530

    Q6

---
+++ 1.Arrays/3.Sliding Window pattern/2.Variable Window/_4_longest_subarray_with_sum_k.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class _4_longest_subarray_with_sum_k {
+    public static void main(String[] args) {
+            // Description
+            // Find the longest substring with exactly k unique characters.
+            // Sample Input
+            // aabacbebebe
+            // 3
+            // Sample Output
+            // 7
+            // Hint
+            // Expand until distinct characters exceed k.
+            Scanner sc=new Scanner(System.in);
+            String s=sc.next();
+            int k=sc.nextInt();
+            
+    }
+}
