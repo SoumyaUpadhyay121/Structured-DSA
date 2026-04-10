416fd052e8689ee96f3599a2350c8d3a0e91838f
Soumya Upadhyay <soumyaupadhya121@gmail.com>
2026-04-10 18:13:50 +0530

    Q6

--- 1.Arrays/3.Sliding Window pattern/2.Variable Window/_4_longest_subarray_with_sum_k.java
+++ 1.Arrays/3.Sliding Window pattern/2.Variable Window/_4_longest_subarray_with_sum_k.java
@@ -17,9 +17,20 @@ public class _4_longest_subarray_with_sum_k {
             HashMap<Character,Integer> map=new HashMap<>();
             int l=0,r=0,maxlen=0;
             while(r<s.length()){
-                map.put(s.charAt(r),map.getOrDefault(s.charAt(r),0)+1);
-                
-                
+                char ch=s.charAt(r);
+                map.put(ch,map.getOrDefault(ch,0)+1);
+                if(map.size()>k){
+                    map.put(s.charAt(l),map.get(s.charAt(l))-1);
+                    if(map.get(s.charAt(l))==0){
+                        map.remove(s.charAt(l));
+                    }
+                    l++;
+                }
+                if(map.size()==k){
+                    maxlen=Math.max(maxlen,r-l+1);
+                }
+                r++;
             }
+            System.out.println(maxlen);
     }
 }
