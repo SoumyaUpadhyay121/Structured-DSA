c9b059fabde0e7890e8da1a9f4090e844fa4830e
Soumya Upadhyay <soumyaupadhya121@gmail.com>
2026-04-12 13:50:05 +0530

    updated

--- 1.Arrays/3.Sliding Window pattern/2.Variable Window/_6_max_vowel_in_arr.java
+++ 1.Arrays/3.Sliding Window pattern/2.Variable Window/_6_max_vowel_in_arr.java
@@ -1,15 +1,39 @@
+import java.util.*;
+
 public class _6_max_vowel_in_arr {
     public static void main(String[] args) {
-        // Description
-        // Count the number of subarrays with sum equal to k.
+        //Description
+        // Find the maximum number of vowels in any substring.
         // Sample Input
-        // 5
-        // 1 2 1 2 1
+        // abciiidef
         // 3
         // Sample Output
-        // 4
-
+        // 3
         // Hint
-        //Adjust window size based on sum.
+        // Track vowels in current window.
+        Scanner sc=new Scanner(System.in);
+        String s=sc.next();
+        int k=sc.nextInt();
+        int count=0;
+        for(int i=0;i<k;i++){
+            if(vowel(s.charAt(i))){
+                count++;
+            }
+        }
+        int max=count;
+        for(int i=k;i<s.length();i++){
+            if(vowel(s.charAt(i-k))){
+                count--;
+            }
+            if(vowel(s.charAt(i))){
+                count++;
+            }
+            max=Math.max(count,max);
+        }
+        System.out.println(max);
+    }
+    public static boolean vowel(char c){
+         return c == 'a' || c == 'e' || c == 'i' 
+            || c == 'o' || c == 'u';
     }
 }
