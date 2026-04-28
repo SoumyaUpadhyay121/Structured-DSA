f769f6f90cb23dcc8e3334c5fcf5310003e34bd5
Soumya Upadhyay <soumyaupadhya121@gmail.com>
2026-04-28 20:34:17 +0530

    Q2

--- 1.Arrays/4.Prefix Sum Pattern/2. Range Sum Queries/_0_between_range.java
+++ 1.Arrays/4.Prefix Sum Pattern/2. Range Sum Queries/_0_between_range.java
@@ -1,5 +1,31 @@
+import java.util.Scanner;
+
 public class _0_between_range{
     public static void main(String[] args) {
-        
+        // Description
+        // Given an array and indices L and R , find the sum of elements between them (inclusive).
+        // Sample Input
+        // 5
+        // 1 2 3 4 5
+
+        // 1 3
+        // Sample Output
+        // 9
+        // Hint
+        // Use prefix[R] - prefix[L-1]
+        Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt();
+        int arr[]=new int[n];
+        for(int i=0;i<n;i++){
+            arr[i]=sc.nextInt();
+        }
+        int l=sc.nextInt();
+        int r=sc.nextInt();
+        int sum=0;
+        for(int i=l;i<=r;i++){
+            sum+=arr[i];
+        }
+        System.out.println(sum);
+
     }
 }
