d6649dbd37a6e7085f7a73a74cb70c816dcc7b9f
Soumya Upadhyay <soumyaupadhya121@gmail.com>
2026-05-14 21:10:58 +0530

    Q10

--- 1.Arrays/4.Prefix Sum Pattern/2. Range Sum Queries/_7_min_sum_range.java
+++ 1.Arrays/4.Prefix Sum Pattern/2. Range Sum Queries/_7_min_sum_range.java
@@ -1,5 +1,44 @@
+import java.util.Scanner;
+
 public class _7_min_sum_range {
     public static void main(String[] args) {
-        
+        //  Description
+        // Find the range with minimum sum among given queries.
+        // Sample Input
+        // 5
+        // 5 4 3 2 1
+        // 2
+        // 0 2
+        // 2 4
+        // Sample Output
+        // 6
+        // Hint
+        // Compare range sums.
+         Scanner sc=new Scanner(System.in);
+
+        int n=sc.nextInt();
+        int arr[]=new int[n];
+
+        for(int i=0;i<n;i++){
+            arr[i]=sc.nextInt();
+        }
+
+        int q=sc.nextInt();
+        int min=Integer.MAX_VALUE;
+
+        while(q-->0){
+            int l=sc.nextInt();
+            int r=sc.nextInt();
+
+            int sum=0;
+
+            for(int i=l;i<=r;i++){
+                sum+=arr[i];
+            }
+
+            min=Math.min(min,sum);
+        }
+
+        System.out.println(min);
     }
 }
